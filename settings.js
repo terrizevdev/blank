faba12750189ca1ad04fc8af445eaf6d5b7ecb30
terrizevdev <terrizevdev@gmail.com>
2025-10-17 12:46:05 +0300

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,14 @@
+module.exports = {
+  name: {
+    main: "Terri API",
+    copyright: "Terri"
+  },
+  description: "Integrated API solution for your modern application development needs. Fast, secure, free and reliable access.",
+  icon: "/image/icon.png",
+  author: "Terri",
+  info_url: "https://whatsapp.com/channel/0029Vb57ZHh7IUYcNttXEB3y",
+  links: [{
+    name: "Channel WhatsApp Inf.",
+    url: "https://whatsapp.com/channel/0029Vb57ZHh7IUYcNttXEB3y"
+  }]
+};
